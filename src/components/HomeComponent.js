ea04182242a42102566e508d7b3fe0c650869d6a
Christopher Torres <ctorresj08@gmail.com>
2021-12-07 15:22:34 -0600

    Move LocationList into Home for easier state management

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,20 +1,50 @@
 import React from 'react';
-import Map from './LocationMapComponent';
-import { HISTORY } from '../shared/history';
-import { Container, Row, Col } from 'reactstrap';
+import Map from './MapComponent';
+import { Row, Col, Container, Card, CardBody } from 'reactstrap';
+
+
 
 function Home(props) {
+
+    const [bounceState, updateBounceState] = React.useState(props.history.map(i => {return (false)}));
+
+    const handleCardClick = (index) => {
+        // Create a fully false state to prevent multiple markers from bouncing
+        let freshState = bounceState.map(i => false);
+        freshState[index] = !bounceState[index];
+        updateBounceState([...freshState]);
+    }
+
+    const LocationList = props.history.map((i, index) => {
+        return (
+            <Card key={index} className="mx-auto" onClick={() => handleCardClick(index)}>
+                <Container>
+                    <Row>
+                        <Col xs="2" className="mx-auto my-auto" style={{ backgroundColor: 'yellow' }}>
+                            {index}
+                        </Col>
+                        <Col xs="10" >
+                            <CardBody>
+                                <div>{i.title}</div>
+                                <address>{i.address}<br /> {i.city}, {i.state} {i.zip}</address>
+                                <div>On {i.date}</div>
+                                <div> @ {i.hours}</div>
+                            </CardBody>
+                        </Col>
+                    </Row>
+                </Container>
+            </Card>
+        );
+    });
 
     return (
         <Container>
             <Row className="mt-5 mb-5">
-                <Col>
-                    <Map history={HISTORY} />
+                <Col xs="12" lg="8">
+                    <Map history={props.history} bounce={bounceState}/>
                 </Col>
-            </Row>
-            <Row>
                 <Col>
-                    <div />
+                    {LocationList}
                 </Col>
             </Row>
         </Container>
