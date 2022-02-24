b3affac7c79b47e2f81fb004ed5d36f5d17d844e
Christopher Torres <ctorresj08@gmail.com>
2022-02-24 13:27:44 -0600

    Add horizontal list to map

--- src/components/LocationListComponent.js
+++ src/components/LocationListComponent.js
@@ -1,35 +1,115 @@
-import React from 'react';
-import { Row, Col, Container, Card, CardBody } from 'reactstrap';
+import React, { useState, useRef } from "react";
+import { Card, CardBody, CardTitle, CardText } from "reactstrap";
+import {
+  BsFillArrowLeftSquareFill,
+  BsFillArrowRightSquareFill
+} from "react-icons/bs";
+import Map from "./MapComponent";
 
-function LocationList(props) {
+const ListItems = ({ history, historyIsLoading, errMess }) => {
+  if (historyIsLoading) {
+    return <div />;
+  }
 
-    const historyList = props.history.map(i => {
-        return (
-            <Card className="mx-auto" onClick={}>
-                <Container>
-                <Row>
-                    <Col xs="2" className="mx-auto my-auto" style={{backgroundColor: 'yellow'}}>
-                        1
-                    </Col>
-                    <Col xs="10" >
-                        <CardBody>
-                            <div>{i.title}</div>
-                            <address>{i.address}<br /> {i.city}, {i.state} {i.zip}</address>
-                            <div>On {i.date}</div>
-                            <div> @ {i.hours}</div>
-                        </CardBody>
-                    </Col>
-                </Row>
-                </Container>
-            </Card>
-        );
+  if (history) {
+    return history.map(i => {
+      return (
+        <li key={"location-list-" + i}>
+          <Card>
+            <CardBody className="ll-card-body">
+              <CardTitle className="ll-card-title">{i.title}</CardTitle>
+              <CardText className="ll-card-text">
+                {i.address + "\n" + i.city + ", " + i.state + " " + i.zip}
+              </CardText>
+              <CardText className="ll-card-text">
+                {"Date: " + i.date}
+              </CardText>
+              <CardText className="ll-card-text">
+                {"Hours: " + i.hours}
+              </CardText>
+            </CardBody>
+          </Card>
+        </li>
+      );
     });
+  }
+};
 
-    return (
-        <>
-            {historyList}
-        </>
-    );
-}
+const LocationList = props => {
+  let scrl = useRef(null);
+  const [scrollX, setscrollX] = useState(0);
+  const [scrolEnd, setscrolEnd] = useState(false);
+  const [bounceState, setBounce] = useState(0);
+
+  //Slide click
+  const slide = shift => {
+    scrl.current.scrollLeft += shift;
+    setscrollX(scrollX + shift);
+
+    if (
+      Math.floor(scrl.current.scrollWidth - scrl.current.scrollLeft) <=
+      scrl.current.offsetWidth
+    ) {
+      setscrolEnd(true);
+    } else {
+      setscrolEnd(false);
+    }
+  };
+
+  const scrollCheck = () => {
+    setscrollX(scrl.current.scrollLeft);
+    if (
+      Math.floor(scrl.current.scrollWidth - scrl.current.scrollLeft) <=
+      scrl.current.offsetWidth
+    ) {
+      setscrolEnd(true);
+    } else {
+      setscrolEnd(false);
+    }
+  };
+
+  return (
+    <div
+      style={{
+        width: "100vw",
+        maxWidth: "750px",
+        position: "relative",
+        boxShadow: "0px 0px 8px 0px #888888"
+      }}
+    >
+      <div>
+        <Map
+          history={props.history}
+          historyIsLoading={props.historyLoading}
+          errMess={props.historyFailed}
+          bounce={bounceState}
+        />
+      </div>
+      <div className="location-list-container">
+        <button
+          className="prev"
+          onClick={() => slide(-125)}
+          onContextMenu={e => e.preventDefault()}
+        >
+          <BsFillArrowLeftSquareFill />
+        </button>
+        <ul ref={scrl} onScroll={scrollCheck}>
+          <ListItems
+            history={props.history}
+            historyIsLoading={props.historyLoading}
+            errMess={props.historyFailed}
+          />
+        </ul>
+        <button
+          className="next"
+          onClick={() => slide(+125)}
+          onContextMenu={e => e.preventDefault()}
+        >
+          <BsFillArrowRightSquareFill />
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default LocationList;
