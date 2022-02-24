5041c8fa7a4ac08c3293264512a58a68a2be3c76
Christopher Torres <ctorresj08@gmail.com>
2022-02-24 13:26:41 -0600

    Add local network baseUrl

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -44,73 +44,79 @@ export const historyLoading = () => ({
 export const fetchMenu = () => dispatch => {
   dispatch(menuLoading());
 
-  return fetch(baseUrl + 'menu')
-      .then(response => {
-          if (response.ok) {
-              return response;
-          } else {
-              const error = new Error(`Error ${response.status}: ${response.statusText}`);
-              error.response = response;
-              throw error;
-          }
+  return fetch(baseUrl + "menu")
+    .then(
+      response => {
+        if (response.ok) {
+          return response;
+        } else {
+          const error = new Error(
+            `Error ${response.status}: ${response.statusText}`
+          );
+          error.response = response;
+          throw error;
+        }
       },
-          error => {
-              const errMess = new Error(error.message);
-              throw errMess;
-          }
-      )
-      .then(response => response.json())
-      .then(menu => dispatch(addMenu(menu)))
-      .catch(error => dispatch(menuFailed(error.message)));
-}
+      error => {
+        const errMess = new Error(error.message);
+        throw errMess;
+      }
+    )
+    .then(response => response.json())
+    .then(menu => dispatch(addMenu(menu)))
+    .catch(error => dispatch(menuFailed(error.message)));
+};
 
 export const addMenu = menu => ({
   type: ActionTypes.ADD_MENU,
   payload: menu
-})
+});
 
 export const menuFailed = errMess => ({
   type: ActionTypes.MENU_FAILED,
   payload: errMess
-})
+});
 
 export const menuLoading = () => ({
   type: ActionTypes.MENU_LOADING
-})
+});
 
 export const fetchIngredient = () => dispatch => {
   dispatch(ingredientLoading());
 
-  return fetch(baseUrl + 'ingredient')
-      .then(response => {
-          if (response.ok) {
-              return response;
-          } else {
-              const error = new Error(`Error ${response.status}: ${response.statusText}`);
-              error.response = response;
-              throw error;
-          }
+  return fetch(baseUrl + "ingredient")
+    .then(
+      response => {
+        if (response.ok) {
+          return response;
+        } else {
+          const error = new Error(
+            `Error ${response.status}: ${response.statusText}`
+          );
+          error.response = response;
+          throw error;
+        }
       },
-          error => {
-              const errMess = new Error(error.message);
-              throw errMess;
-          }
-      )
-      .then(response => response.json())
-      .then(menu => dispatch(addIngredient(menu)))
-      .catch(error => dispatch(ingredientFailed(error.message)));
-}
+      error => {
+        const errMess = new Error(error.message);
+        throw errMess;
+      }
+    )
+    .then(response => response.json())
+    .then(ingredient => dispatch(addIngredient(ingredient)))
+    .catch(error => dispatch(ingredientFailed(error.message)));
+};
 
 export const addIngredient = ingredient => ({
   type: ActionTypes.ADD_INGREDIENT,
   payload: ingredient
-})
+});
 
 export const ingredientFailed = errMess => ({
   type: ActionTypes.INGREDIENT_FAILED,
   payload: errMess
-})
+});
 
 export const ingredientLoading = () => ({
   type: ActionTypes.INGREDIENT_LOADING
-})
+});
