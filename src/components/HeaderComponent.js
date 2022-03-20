833b5a14a90689fb46950c25862a7439e6c701ae
Christopher Torres <ctorresj08@gmail.com>
2022-03-20 13:03:41 -0500

    Add header to page for navigation

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,85 +1,92 @@
-import React, { Component } from 'react';
-import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Jumbotron} from 'reactstrap';
-import { NavLink } from 'react-router-dom';
+import React, { Component } from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  Nav,
+  NavbarToggler,
+  Collapse,
+  NavItem
+} from "reactstrap";
+import logo from "../images/TBC-Logo-short.svg";
+
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props);
+    this.toggleNav = this.toggleNav.bind(this);
+    this.state = {
+      isNavOpen: false,
+      isModalOpen: false
+    };
 
-        this.toggleNav = this.toggleNav.bind(this);
-        this.state = {
-            isNavOpen: false,
-            isModalOpen: false
-        };
+    this.toggleNav = this.toggleNav.bind(this);
+    this.toggleModal = this.toggleModal.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+  }
 
-        this.toggleNav = this.toggleNav.bind(this);
-        this.toggleModal = this.toggleModal.bind(this);
-        this.handleLogin = this.handleLogin.bind(this);
+  toggleNav() {
+    this.setState({
+      isNavOpen: !this.state.isNavOpen
+    });
+  }
 
-    }
+  toggleModal() {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen
+    });
+  }
 
-    toggleNav() {
-        this.setState({
-            isNavOpen: !this.state.isNavOpen
-        });
-    }
+  handleLogin(event) {
+    alert(
+      `Username: ${this.username.value} Password: ${this.password
+        .value} Remember: ${this.remember.checked}`
+    );
+    this.toggleModal();
+    event.preventDefault();
+  }
 
-    toggleModal() {
-        this.setState({
-            isModalOpen: !this.state.isModalOpen
-        });
-    }
-
-    handleLogin(event) {
-        alert(`Username: ${this.username.value} Password: ${this.password.value} Remember: ${this.remember.checked}`);
-        this.toggleModal();
-        event.preventDefault();
-    }
-
-    render() {
-        return (
-            <React.Fragment>
-
-                <Navbar dark sticky="top" expand="md">
-                    <div className="container">
-                        <NavbarBrand className="ml-auto" href="/">Patty Plate</NavbarBrand>
-                        <NavbarToggler onClick={this.toggleNav} />
-                        <Collapse isOpen={this.state.isNavOpen} navbar>
-                            <Nav navbar>
-                                <NavItem>
-                                    <NavLink className="nav-link" to="/home">
-                                        <i className="fa fa-home fa-lg" /> Home
-                                    </NavLink>
-                                </NavItem>
-                                <NavItem>
-                                    <NavLink className="nav-link" to="/menu">
-                                        <i className="fa fa-list fa-lg" /> Menu
-                                    </NavLink>
-                                </NavItem>
-                                <NavItem>
-                                    <NavLink className="nav-link" to="/burgerbuilder">
-                                        <i className="fa fa-list fa-lg" /> Burger Builder
-                                    </NavLink>
-                                </NavItem>
-                                <NavItem>
-                                    <NavLink className="nav-link" to="/requeststop">
-                                        <i className="fa fa-info fa-lg" /> Request Stop
-                                    </NavLink>
-                                </NavItem>
-                                <NavItem>
-                                    <NavLink className="nav-link" to="/componentsort">
-                                        <i className="fa fa-info fa-lg" /> Sort
-                                    </NavLink>
-                                </NavItem>
-                            </Nav>
-                        </Collapse>
-
-                    </div>
-                </Navbar>
-            </React.Fragment>
-        );
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar dark sticky="top" expand="md" className="header-navbar">
+          <div className="container">
+            <NavbarBrand className="mr-auto" href="#"><img src={logo} className="header-img" alt="Torres Burger Co." /></NavbarBrand>
+            <NavbarToggler onClick={this.toggleNav} />
+            <Collapse isOpen={this.state.isNavOpen} navbar>
+              <Nav navbar>
+                <NavItem>
+                  <a className="nav-link" href="#sec1">
+                    <i className="fa fa-list fa-lg" /> Menu
+                  </a>
+                </NavItem>
+                <NavItem>
+                  <a className="nav-link" href="#sec2">
+                    <i className="fa fa-info fa-lg" /> Location
+                  </a>
+                </NavItem>
+                <NavItem>
+                  <a className="nav-link" href="#sec3">
+                    <i className="fa fa-info fa-lg" /> Request
+                  </a>
+                </NavItem>
+                <NavItem>
+                  <a className="nav-link" href="#sec4">
+                    <i className="fa fa-info fa-lg" /> About Us
+                  </a>
+                </NavItem>
+                <NavItem>
+                  <a className="nav-link" href="#">
+                    <i className="fa fa-info fa-lg" /> Back to top
+                  </a>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </div>
+        </Navbar>
+      </React.Fragment>
+    );
+  }
 }
 
 export default Header;
